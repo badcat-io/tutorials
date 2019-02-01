f29ed8d8e4cfb660578a1222619a3d39165e54b0
Sapun4ik <sapun4ik@gmail.com>
2019-02-02 00:06:33 +0300

    add spring-xml-standelone-app

--- spring-xml-standelone-app/src/main/java/io/badcat/main/Application.java
+++ spring-xml-standelone-app/src/main/java/io/badcat/main/Application.java
@@ -9,11 +9,11 @@ public class Application {
     
     public static void main(String[] args) {
         
-       ApplicationContext context = new ClassPathXmlApplicationContext("my-beans.xml");
- 
-       Message obj = (Message) context.getBean("mymessage");
- 
-       String msg = obj.getMessage();
-       System.out.println(msg);
+        ApplicationContext context = new ClassPathXmlApplicationContext("my-beans.xml");
+        Message obj = (Message) context.getBean("mymessage");
+
+        String msg = obj.getMessage();
+        System.out.println(msg);
+
     }
- }
+}
